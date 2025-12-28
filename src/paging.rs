3f2b05311b9976709083a8cb16a60bc9da4089bc
Erhan Bağdemir <erhan@reevik.net>
2025-12-28 12:20:08 +0100

    Slot insertion complete

--- src/paging.rs
+++ src/paging.rs
@@ -1,14 +1,19 @@
-use std::clone;
-
 use alloc::vec::Vec;
 
 type PageId = u32;
 
-// TODO this needs to be persisted in a configuration file.
-static mut next_page_id:u32 = 0;
-const PAGE_SIZE: usize = 4096;
+/// ***PSize*** is used to access the page regions, which is the offset type. Its size also
+/// limits the maximum page size.
+type PSize = u16;
 
-const SIZE_NUM_OF_SLOTS: usize = size_of::<u16>();
+enum PageError {
+    NoSpace,
+}
+// TODO this needs to be persisted in a configuration file.
+static mut NEXT_PAGE_ID: u32 = 0;
+const PAGE_SIZE: PSize = 4096;
+
+const SIZE_NUM_OF_SLOTS: usize = size_of::<PSize>();
 const SIZE_PAGE_ID: usize = size_of::<PageId>();
 const SIZE_PAGE_TYPE: usize = size_of::<u8>();
 const SIZE_FLAGS: usize = size_of::<u8>();
@@ -16,8 +21,9 @@ const SIZE_LEFT_MOST: usize = size_of::<PageId>();
 const SIZE_LEFT_SIBLING: usize = size_of::<PageId>();
 const SIZE_RIGHT_SIBLING: usize = size_of::<PageId>();
 const SIZE_PARENT_PAGE_ID: usize = size_of::<PageId>();
-const SIZE_FREE_START: usize = size_of::<u16>();
-const SIZE_FREE_END: usize = size_of::<u16>();
+const SIZE_FREE_START: usize = size_of::<PSize>();
+const SIZE_FREE_END: usize = size_of::<PSize>();
+const SIZE_OF_SLOT_TABLE_ITEM: usize = size_of::<PSize>();
 
 const TOTAL_HEADER_SIZE: usize = SIZE_FLAGS
     + SIZE_RIGHT_SIBLING
@@ -41,21 +47,30 @@ const OFFSET_FREE_START: usize = OFFSET_PARENT_PAGE_ID + SIZE_FREE_START;
 const OFFSET_FREE_END: usize = OFFSET_FREE_START + SIZE_FREE_START;
 
 pub struct SlottedPage {
-    buffer: [u8; PAGE_SIZE],
+    buffer: [u8; PAGE_SIZE as usize],
 }
 
-struct Slot {
-    buffer: Vec<u8>
+trait PagePayload {
+    const SIZE: usize;
+    fn to_le_bytes(&self) -> Vec<u8>;
+}
+
+impl PagePayload for PageId {
+    const SIZE: usize = size_of::<PageId>();
+
+    fn to_le_bytes(&self) -> Vec<u8> {
+        PageId::to_le_bytes(*self).to_vec()
+    }
 }
 
 enum PageType {
     INNER,
-    LEAF
+    LEAF,
 }
 
 impl From<u8> for PageType {
     fn from(value: u8) -> Self {
-        if (value == 0) {
+        if value == 0 {
             return Self::INNER;
         }
         Self::LEAF
@@ -63,10 +78,9 @@ impl From<u8> for PageType {
 }
 
 impl SlottedPage {
-
     fn new() -> Self {
         let mut new_instance = Self {
-            buffer: [0u8; PAGE_SIZE],
+            buffer: [0u8; PAGE_SIZE as usize],
         };
 
         new_instance.set_flags(0);
@@ -75,44 +89,77 @@ impl SlottedPage {
         new_instance.set_left_sibling(0);
         new_instance.set_parent(0);
         new_instance.set_num_of_slots(0);
-        new_instance.set_free_start(PAGE_SIZE as u16);
-        new_instance.set_free_end(TOTAL_HEADER_SIZE as u16);
+        new_instance.set_free_start(TOTAL_HEADER_SIZE as PSize);
+        new_instance.set_free_end(PAGE_SIZE as PSize);
         new_instance.set_page_type(0);
         unsafe {
-            new_instance.set_page_id(next_page_id);
-            next_page_id += 1;
+            new_instance.set_page_id(NEXT_PAGE_ID);
+            NEXT_PAGE_ID += 1;
         }
         new_instance
     }
 
     pub fn new_leaf() -> Self {
-       let mut instance = Self::new();
-       instance.set_page_type(0);
-       instance
+        let mut instance = Self::new();
+        instance.set_page_type(0);
+        instance
     }
 
     pub fn new_inner() -> Self {
-       let mut instance = Self::new();
-       instance.set_page_type(1);
-       instance
+        let mut instance = Self::new();
+        instance.set_page_type(1);
+        instance
     }
 
     pub fn add_left_most(&mut self, left_most_page_id: PageId) {
-        self.set_left_most_page_id(left_most_page_id); 
+        self.set_left_most_page_id(left_most_page_id);
     }
 
-    pub fn add_key_ref(&mut self, key: &str, page_id: PageId) {
+    fn add_key_ref<T: PagePayload>(&mut self, key: &str, ref_id: T) -> Result<(), PageError> {
         let key_bytes = key.as_bytes();
-        let slot_payload_len = key_bytes.len() + size_of::<PageId>();
-        let slot_buffer = &mut slot_payload_len.to_le_bytes();
-        slot_buffer.extend(key_bytes);
-        slot_buffer.extend_from_slice(&page_id.to_le_bytes());
-        if usize::from(self.free_end() - self.free_start()) < slot_buffer.len() {
-            panic!("No available space in the page.")
-        }
-        let slot_arr:[u8::slot_buffer.len()] = slot_buffer.iter().try_into().expect("wrong length");
-        self.buffer[free_end - slot_arr.len()..free_end].copy_from_slice(&slot_arr);
+        let slot_payload_len = (key_bytes.len() + size_of::<T>()) as PSize;
+        let mut slot: Vec<u8> = Vec::with_capacity(Self::slot_size::<T>(key) as usize);
+        //  ... < | key | payload | header | key | payload | header | <- MAX_PAGE_SIZE
+        slot.extend_from_slice(key_bytes);
+        slot.extend_from_slice(&ref_id.to_le_bytes());
+        slot.extend_from_slice(&slot_payload_len.to_le_bytes());
 
+        let required_space = slot.len() + SIZE_OF_SLOT_TABLE_ITEM;
+        if self.available_size() < required_space as PSize {
+            return Err(PageError::NoSpace);
+        }
+        let new_free_end = self.add_slot(&mut slot);
+        // advance the free start and slot table with the new free end.
+        self.add_to_slot_table(&new_free_end);
+        Ok(())
+    }
+
+    fn slot_size<T: PagePayload>(key: &str) -> PSize {
+        (size_of::<PSize>() + key.as_bytes().len() + size_of::<T>()) as PSize
+    }
+
+    fn add_to_slot_table(&mut self, new_free_end: &PSize) {
+        let free_start = self.free_start();
+        let new_free_end_offset = &new_free_end.to_le_bytes();
+        self.buffer[free_start as usize..free_start as usize + SIZE_OF_SLOT_TABLE_ITEM]
+            .copy_from_slice(new_free_end_offset);
+        self.set_free_start(free_start + SIZE_OF_SLOT_TABLE_ITEM as PSize);
+        self.set_num_of_slots(self.num_of_slots() + 1);
+        debug_assert!(self.free_start() <= self.free_end());
+    }
+
+    fn add_slot(&mut self, slot: &Vec<u8>) -> PSize {
+        let free_end = self.free_end();
+        let new_free_end = free_end - slot.len() as PSize;
+        // update the buffer with key-payload.
+        self.buffer[new_free_end as usize..free_end as usize].copy_from_slice(&slot);
+        self.set_free_end(new_free_end);
+        debug_assert!(self.free_start() <= self.free_end());
+        new_free_end
+    }
+
+    pub(crate) fn available_size(&self) -> PSize {
+        self.free_end().saturating_sub(self.free_start())
     }
 
     fn read_le<T, const N: usize>(buf: &[u8], offset: usize, f: fn([u8; N]) -> T) -> T {
@@ -127,33 +174,33 @@ impl SlottedPage {
     }
 
     /// Returns the number of slots from the first two bytes in the page.
-    fn num_of_slots(&self) -> u16 {
-        Self::read_le::<u16, SIZE_NUM_OF_SLOTS>(
+    fn num_of_slots(&self) -> PSize {
+        Self::read_le::<PSize, SIZE_NUM_OF_SLOTS>(
             &self.buffer,
             OFFSET_NUM_OF_SLOTS,
-            u16::from_le_bytes,
+            PSize::from_le_bytes,
         )
     }
 
-    fn set_num_of_slots(&mut self, num: u16) {
-        Self::write_le::<u16, SIZE_NUM_OF_SLOTS>(
+    fn set_num_of_slots(&mut self, num: PSize) {
+        Self::write_le::<PSize, SIZE_NUM_OF_SLOTS>(
             &mut self.buffer,
             OFFSET_NUM_OF_SLOTS,
             num,
-            u16::to_le_bytes,
+            PSize::to_le_bytes,
         );
     }
 
-    fn page_id(&self) -> u32 {
-        Self::read_le::<u32, SIZE_PAGE_ID>(&self.buffer, OFFSET_PAGE_ID, u32::from_le_bytes)
+    fn page_id(&self) -> PageId {
+        Self::read_le::<PageId, SIZE_PAGE_ID>(&self.buffer, OFFSET_PAGE_ID, u32::from_le_bytes)
     }
 
-    fn set_page_id(&mut self, num: u32) {
-        Self::write_le::<u32, SIZE_PAGE_ID>(
+    fn set_page_id(&mut self, num: PageId) {
+        Self::write_le::<PageId, SIZE_PAGE_ID>(
             &mut self.buffer,
             OFFSET_PAGE_ID,
             num,
-            u32::to_le_bytes,
+            PageId::to_le_bytes,
         );
     }
 
@@ -178,93 +225,136 @@ impl SlottedPage {
         Self::write_le::<u8, SIZE_FLAGS>(&mut self.buffer, OFFSET_FLAGS, num, u8::to_le_bytes);
     }
 
-    fn left_most_page_id(&self) -> u32 {
-        Self::read_le::<u32, SIZE_LEFT_MOST>(&self.buffer, OFFSET_LEFT_MOST, u32::from_le_bytes)
+    fn left_most_page_id(&self) -> PageId {
+        Self::read_le::<PageId, SIZE_LEFT_MOST>(
+            &self.buffer,
+            OFFSET_LEFT_MOST,
+            PageId::from_le_bytes,
+        )
     }
 
-    fn set_left_most_page_id(&mut self, num: u32) {
-        Self::write_le::<u32, SIZE_LEFT_MOST>(
+    fn set_left_most_page_id(&mut self, num: PageId) {
+        Self::write_le::<PageId, SIZE_LEFT_MOST>(
             &mut self.buffer,
             OFFSET_LEFT_MOST,
             num,
-            u32::to_le_bytes,
+            PageId::to_le_bytes,
         );
     }
 
-    fn left_sibling(&self) -> u32 {
-        Self::read_le::<u32, SIZE_LEFT_SIBLING>(
+    fn left_sibling(&self) -> PageId {
+        Self::read_le::<PageId, SIZE_LEFT_SIBLING>(
             &self.buffer,
             OFFSET_LEFT_SIBLING,
-            u32::from_le_bytes,
+            PageId::from_le_bytes,
         )
     }
 
-    fn set_left_sibling(&mut self, num: u32) {
-        Self::write_le::<u32, SIZE_LEFT_SIBLING>(
+    fn set_left_sibling(&mut self, num: PageId) {
+        Self::write_le::<PageId, SIZE_LEFT_SIBLING>(
             &mut self.buffer,
             OFFSET_LEFT_SIBLING,
             num,
-            u32::to_le_bytes,
+            PageId::to_le_bytes,
         );
     }
 
-    fn right_sibling(&self) -> u32 {
-        Self::read_le::<u32, SIZE_RIGHT_SIBLING>(
+    fn right_sibling(&self) -> PageId {
+        Self::read_le::<PageId, SIZE_RIGHT_SIBLING>(
             &self.buffer,
             OFFSET_RIGHT_SIBLING,
-            u32::from_le_bytes,
+            PageId::from_le_bytes,
         )
     }
 
-    fn set_right_sibling(&mut self, num: u32) {
-        Self::write_le::<u32, SIZE_RIGHT_SIBLING>(
+    fn set_right_sibling(&mut self, num: PageId) {
+        Self::write_le::<PageId, SIZE_RIGHT_SIBLING>(
             &mut self.buffer,
             OFFSET_RIGHT_SIBLING,
             num,
-            u32::to_le_bytes,
+            PageId::to_le_bytes,
         );
     }
 
-    fn parent(&self) -> u32 {
-        Self::read_le::<u32, SIZE_PARENT_PAGE_ID>(
+    fn parent(&self) -> PageId {
+        Self::read_le::<PageId, SIZE_PARENT_PAGE_ID>(
             &self.buffer,
             OFFSET_PARENT_PAGE_ID,
-            u32::from_le_bytes,
+            PageId::from_le_bytes,
         )
     }
 
-    fn set_parent(&mut self, num: u32) {
-        Self::write_le::<u32, SIZE_PARENT_PAGE_ID>(
+    fn set_parent(&mut self, num: PageId) {
+        Self::write_le::<PageId, SIZE_PARENT_PAGE_ID>(
             &mut self.buffer,
             OFFSET_PARENT_PAGE_ID,
             num,
-            u32::to_le_bytes,
+            PageId::to_le_bytes,
         );
     }
 
-    fn free_start(&self) -> u16 {
-        Self::read_le::<u16, SIZE_FREE_START>(&self.buffer, OFFSET_FREE_START, u16::from_le_bytes)
+    fn free_start(&self) -> PSize {
+        Self::read_le::<PSize, SIZE_FREE_START>(
+            &self.buffer,
+            OFFSET_FREE_START,
+            PSize::from_le_bytes,
+        )
     }
 
-    fn set_free_start(&mut self, num: u16) {
-        Self::write_le::<u16, SIZE_FREE_START>(
+    fn set_free_start(&mut self, num: PSize) {
+        Self::write_le::<PSize, SIZE_FREE_START>(
             &mut self.buffer,
             OFFSET_FREE_START,
             num,
-            u16::to_le_bytes,
+            PSize::to_le_bytes,
         );
     }
 
-    fn free_end(&self) -> u16 {
-        Self::read_le::<u16, SIZE_FREE_END>(&self.buffer, OFFSET_FREE_END, u16::from_le_bytes)
+    fn free_end(&self) -> PSize {
+        Self::read_le::<PSize, SIZE_FREE_END>(&self.buffer, OFFSET_FREE_END, PSize::from_le_bytes)
     }
 
-    fn set_free_end(&mut self, num: u16) {
-        Self::write_le::<u16, SIZE_FREE_END>(
+    fn set_free_end(&mut self, num: PSize) {
+        Self::write_le::<PSize, SIZE_FREE_END>(
             &mut self.buffer,
             OFFSET_FREE_END,
             num,
-            u16::to_le_bytes,
+            PSize::to_le_bytes,
         );
     }
+
+    pub fn print(&self) {
+        let content_as_lossy = String::from_utf8_lossy(&self.buffer);
+        println!("{}", content_as_lossy.to_string());
+    }
+}
+
+#[test]
+fn test_add_slot_results_in_correct_num_of_slots() {
+    let mut new_inner = SlottedPage::new_inner();
+    new_inner.add_key_ref("abc", 123 as PageId);
+    new_inner.add_key_ref("xyz", 789 as PageId);
+    assert_eq!(new_inner.num_of_slots(), 2);
+}
+
+#[test]
+fn verify_available_space_empty_page() {
+    let mut new_inner = SlottedPage::new_inner();
+    let available_space = new_inner.available_size();
+    let total_empty_size = PAGE_SIZE - TOTAL_HEADER_SIZE as PSize;
+    assert_eq!(available_space, total_empty_size as PSize);
+}
+
+#[test]
+fn verify_available_space_after_insertion() {
+    let key = "abc";
+    let mut new_inner = SlottedPage::new_inner();
+    new_inner.add_key_ref(key, 123 as PageId);
+    new_inner.add_key_ref(key, 123 as PageId);
+    let available_space = new_inner.available_size();
+    let total_empty_size = PAGE_SIZE
+        - (TOTAL_HEADER_SIZE as PSize
+            + (2 * SIZE_OF_SLOT_TABLE_ITEM as PSize)
+            + (2 * SlottedPage::slot_size::<PageId>(key)));
+    assert_eq!(available_space, total_empty_size as PSize);
 }
